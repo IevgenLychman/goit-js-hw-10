f80d6703431b5d3776e5ceb0a36173d32dfb3b5f
IevgenLychman <gora.erikovich@gmail.com>
2022-12-08 18:51:07 +0200

    first stage

---
+++ src/index.js
@@ -0,0 +1,52 @@
+import './css/styles.css';
+const DEBOUNCE_DELAY = 300;
+
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
+import createCountryMarkup from './js/create-country-markup.js'
+import createListOfCountryMarkup from './js/create-list.js'
+import fetchCountries  from './js/fetchCountries.js';
+
+const refs = {
+    input: document.querySelector("#search-box"),
+    countryList: document.querySelector(".country-list"),
+    countryDesc: document.querySelector(".country-info"),
+}
+
+refs.input.addEventListener('input', debounce(start, DEBOUNCE_DELAY));
+
+function start(event) {
+    const name = event.target.value.trim()
+     if (name === "") {
+        clearAll()
+    } else {
+    fetchCountries(name).then(country => {
+        if (country.length === 1) {
+            clearAll()
+            refs.countryDesc.innerHTML = createCountryMarkup(country)
+        }
+        else if (country.length > 1 && country.length <= 10) {
+            clearAll()
+            refs.countryList.insertAdjacentHTML('beforeend', createListOfCountryMarkup(country))
+        }
+        else if (country.length > 10) {
+            clearAll()
+            Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+        }
+        else {
+            clearAll()
+            Notiflix.Notify.failure('Oops, there is no country with that name');
+        }
+        
+    }).catch(error => {
+        clearAll()
+        Notiflix.Notify.failure('Oops, there is no country with that name');
+     
+    })
+}
+}
+
+function clearAll() {
+    refs.countryDesc.innerHTML = '';
+    refs.countryList.innerHTML = '';
+}
