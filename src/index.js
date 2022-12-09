426631d5158621fb137f09ffa9003f845892c0f3
IevgenLychman <gora.erikovich@gmail.com>
2022-12-09 20:49:06 +0200

    Finish

--- src/index.js
+++ src/index.js
@@ -3,50 +3,46 @@ const DEBOUNCE_DELAY = 300;
 
 import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
-import createCountryMarkup from './js/create-country-markup.js'
-import createListOfCountryMarkup from './js/create-list.js'
-import fetchCountries  from './js/fetchCountries.js';
+import createCountryMarkup from './js/create-country-markup.js';
+import createListOfCountryMarkup from './js/create-list.js';
+import fetchCountries from './js/fetchCountries.js';
 
 const refs = {
-    input: document.querySelector("#search-box"),
-    countryList: document.querySelector(".country-list"),
-    countryDesc: document.querySelector(".country-info"),
-}
+  input: document.querySelector('#search-box'),
+  countryList: document.querySelector('.country-list'),
+  countryDesc: document.querySelector('.country-info'),
+};
 
 refs.input.addEventListener('input', debounce(start, DEBOUNCE_DELAY));
 
 function start(event) {
-    const name = event.target.value.trim()
-     if (name === "") {
-        clearAll()
-    } else {
-    fetchCountries(name).then(country => {
+  const name = event.target.value.trim();
+  if (name === '') {
+    clearAll();
+  } else {
+    fetchCountries(name)
+      .then(country => {
         if (country.length === 1) {
-            clearAll()
-            refs.countryDesc.innerHTML = createCountryMarkup(country)
+          refs.countryDesc.innerHTML = createCountryMarkup(country);
+        } else if (country.length > 1 && country.length <= 10) {
+          refs.countryList.insertAdjacentHTML(
+            'beforeend',
+            createListOfCountryMarkup(country)
+          );
+        } else if (country.length > 10) {
+          Notiflix.Notify.info(
+            'Too many matches found. Please enter a more specific name.'
+          );
         }
-        else if (country.length > 1 && country.length <= 10) {
-            clearAll()
-            refs.countryList.insertAdjacentHTML('beforeend', createListOfCountryMarkup(country))
-        }
-        else if (country.length > 10) {
-            clearAll()
-            Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
-        }
-        else {
-            clearAll()
-            Notiflix.Notify.failure('Oops, there is no country with that name');
-        }
-        
-    }).catch(error => {
-        clearAll()
+      })
+      .catch(error => {
+        clearAll();
         Notiflix.Notify.failure('Oops, there is no country with that name');
-     
-    })
-}
+      });
+  }
 }
 
 function clearAll() {
-    refs.countryDesc.innerHTML = '';
-    refs.countryList.innerHTML = '';
+  refs.countryDesc.innerHTML = '';
+  refs.countryList.innerHTML = '';
 }
--- src/js/create-country-markup.js
+++ src/js/create-country-markup.js
@@ -1,9 +1,9 @@
-export default function createCountryMarkup(data) {
-    let lang = data[0].languages.map(language => {
-        return language.name;
-    });
-    let languages = lang.join(', ');
-   return `<div class="header-container">
+export default function createCountryMarkup([data]) {
+  let lang = data[0].languages.map(language => {
+    return language.name;
+  });
+  let languages = lang.join(', ');
+  return `<div class="header-container">
    <img src="${data[0].flags.svg}" class="flag" width="50" height="40">
    <h1 class="header">${data[0].name}</h1>
 </div>
