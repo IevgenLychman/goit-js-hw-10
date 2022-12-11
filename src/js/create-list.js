d0e51c2e3947d4f45f2c357c145a562750b248db
IevgenLychman <gora.erikovich@gmail.com>
2022-12-11 19:26:25 +0200

    1

--- src/js/create-list.js
+++
@@ -1,7 +0,0 @@
-export default function createListOfCountryMarkup(info) {
-   
-    return info.map(el => {
-        return `<li class="country-item"><img src="${el.flags.svg}" class="list-img" width="40" height="30">${el.name}</li>`
-    }).join("")
- 
-}
--- src/js/fetchCountries.js
+++
@@ -1,9 +0,0 @@
-export default function fetchCountries(name) {
-  const url = `https://restcountries.com/v2/name/${name}?fields=name,capital,population,flags,languages`;
-  return fetch(url).then(r => {
-    if (!r.ok) {
-      throw new Error(r.status);
-    }
-    return r.json();
-  });
-}
