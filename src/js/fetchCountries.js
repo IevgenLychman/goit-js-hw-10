da00b58d60aed4cf804a6cac195fb598e7ea575d
IevgenLychman <gora.erikovich@gmail.com>
2022-12-11 19:33:54 +0200

    Finish

---
+++ src/js/fetchCountries.js
@@ -0,0 +1,9 @@
+export default function fetchCountries(name) {
+  const url = `https://restcountries.com/v2/name/${name}?fields=name,capital,population,flags,languages`;
+  return fetch(url).then(r => {
+    if (!r.ok) {
+      throw new Error(r.status);
+    }
+    return r.json();
+  });
+}
